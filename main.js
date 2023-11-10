3a9e650e3dd399e59da02287e7793adc1952e90b
Uwe Clement <uwe.clement@exxcellent.de>
2023-11-10 23:27:19 +0100

    dd

--- main.js
+++ main.js
@@ -29,3 +29,39 @@ class Person {
     }
   }
   
+  exports.Person = Person;
+  exports.Schüler = Schüler;
+  exports.Lehrer = Lehrer;
+
+
+  
+/*uwe2();
+exports.getUwe2 = () => {
+    console.log("test9 inside uwe()");
+}
+
+
+function uwe2() {
+    console.log("test9 inside uwe()");
+}
+class Color {
+    constructor(name, code) {
+      this.name = name;
+      this.code = code;
+    }
+  }
+
+  const allColors = [
+    new Color('brightred', '#E74C3C'),
+    new Color('soothingpurple', '#9B59B6'),
+    new Color('skyblue', '#5DADE2'),
+    new Color('leafygreen', '#48C9B0'),
+    new Color('sunkissedyellow', '#F4D03F'),
+    new Color('groovygray', '#D7DBDD'),
+  ];
+
+  exports.getRandomColor = () => {
+    return allColors[Math.floor(Math.random() * allColors.length)];
+  }
+
+  exports.allColors = allColors;*/
