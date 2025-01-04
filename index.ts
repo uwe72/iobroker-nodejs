0c4fe7d9364c3ba084af672ab33ac834f8e187a6
Uwe Clement <uwe.clement@exxcellent.de>
2025-01-04 12:24:05 +0100

    update 20254

--- index.ts
+++ index.ts
@@ -23,7 +23,7 @@ class Buch  {
         } else if (weekday == 7) {
             weekdayAsString = "Sonntag";
         } else if (weekday == 0) {
-            weekdayAsString = "Sonntag";
+            weekdayAsString = "Sonntag2";
         } else {
             weekdayAsString = "???";
         }
